5211b113fbfddfb41748b28be2b19d26e5bfebaa
hsilgnej <j@0xa5.org>
2022-05-16 12:30:25 -0500

    Moved structs

--- raythread.cpp
+++ raythread.cpp
@@ -50,59 +50,13 @@
 #include "raythread.h"
 #include "draw2d.h"
 #include "color.h"
-
+#include "scenefile.h"
 #include "SDL.h"
 
 const float FINF = 4294967296.0;
 const uint32_t BACKGROUND_COLOR = RgbToColor(0x33, 0x33, 0x33);
-const int MAX_THREADS = 8;
+const int MAX_THREADS = 16;
 
-struct viewport_t {
-    float width;
-    float height;
-    float d; //Distance from the camera to the viewport
-};
-
-struct material_t {
-    uint32_t color;
-    int specular;
-    float reflection;
-};
-
-struct sphere_t {
-    v3_t center;
-    float radius;
-};
-
-struct triangle_t {
-    v3_t p1;
-    v3_t p2;
-    v3_t p3;
-};
-
-enum lightType_t {LT_POINT, LT_DIRECTIONAL, LT_AMBIENT};
-enum object_t {OT_SPHERE, OT_TRIANGLE};
-
-struct scene_object_t {
-    object_t type;
-    union{
-        sphere_t sphere;
-        triangle_t triangle;
-    };
-    material_t material;
-};
-
-struct light_t {
-    lightType_t type;
-    float intensity;
-    v3_t position;
-    v3_t direction;
-};
-
-struct camera_t {
-    v3_t position;
-    m3x3_t rotation;
-};
 
 enum worker_status_t {
     WS_READY,
--- scenefile.h
+++ scenefile.h
@@ -4,6 +4,53 @@
 #include <stdlib.h>
 #include "SDL.h"
 
+struct viewport_t {
+    float width;
+    float height;
+    float d; //Distance from the camera to the viewport
+};
+
+struct material_t {
+    uint32_t color;
+    int specular;
+    float reflection;
+};
+
+struct sphere_t {
+    v3_t center;
+    float radius;
+};
+
+struct triangle_t {
+    v3_t p1;
+    v3_t p2;
+    v3_t p3;
+};
+
+enum lightType_t {LT_POINT, LT_DIRECTIONAL, LT_AMBIENT};
+enum object_t {OT_SPHERE, OT_TRIANGLE};
+
+struct scene_object_t {
+    object_t type;
+    union{
+        sphere_t sphere;
+        triangle_t triangle;
+    };
+    material_t material;
+};
+
+struct light_t {
+    lightType_t type;
+    float intensity;
+    v3_t position;
+    v3_t direction;
+};
+
+struct camera_t {
+    v3_t position;
+    m3x3_t rotation;
+};
+
 inline void
 ParseSceneFile(char *filename){
     char s[1024];
