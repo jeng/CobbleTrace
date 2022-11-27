03652224e92b605859d1f60904b47d63781b205d
Jeremy English <jhe@jeremyenglish.org>
2022-11-27 12:42:28 -0600

    I've fixed a typo in the linux build script.

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-g++ cobbletrace.cpp raythread.cpp eventQueue.cpp scenefile.cpp rasterizeScene.cpp bvh.cpp objectLoader.cpp cstring.cpp -lm -lSDL2 -I /usr/include/SDL2 -o cobbletrace
+g++ cobbletrace.cpp raythread.cpp eventQueue.cpp scenefile.cpp rasterizeScene.cpp bvh.cpp objectLoader.cpp ctstring.cpp -lm -lSDL2 -I /usr/include/SDL2 -o cobbletrace
